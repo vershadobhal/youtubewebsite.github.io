fafc42add5553fdbd2678f6b3e69af3d1dc3d8f9
vershadobhal <93519210+vershadobhal@users.noreply.github.com>
2021-11-01 16:40:40 +0530

    publising my first website

---
+++ about.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100&display=swap" rel="stylesheet">
+</head>	
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">gymfreaks</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="services.php">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.php">Contact</a>
+        </li>
+        <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="jumbotron">
+  <h1>Bootstrap Tutorial</h1>
+  <p>Bootstrap is the most popular HTML, CSS...</p>
+</div>
+
+
+<section class="my-5">
+ <div class="py-5">
+  <h2 class="text-center">About us</h2>
+  </div>
+
+<div class="container-fluid">
+    <div class="row">
+    <div class="col-lg-6 col-md-6 col-12">
+     <img src="images/laptop.jpg" class="img-fluid aboutimg">
+    </div>
+    <div class="col-lg-6 col-md-6 col-12">
+     <h2 class="display-4"> Hello There All Of You!</h2>
+     <p class="py-3">In March this year, Molly-Mae explained during one of her vlogs that she'd "completely flipped [her] life upside down in terms of everything - exercise, food, water intake." She added, "I have been on a journey with my body, finding out what works for my body, what I need for my body to feel good." She also added</p>
+     <a href="about.php" class="btn btn-success">check more</a>
+    </div>
+    </div>
+</div>
+</section>  
+
+<footer>
+  <p class="py-3 bg-dark text-white text-center">@gymfreakProduction</p>
+</footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100&display=swap" rel="stylesheet">
+</head>	
+<body>
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">gymfreaks</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="services.php">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.php">Contact</a>
+        </li>
+        <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/slider1.jpg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>We had such a great time in LA!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/slider2.jpg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/adventure-10.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+
+<section class="my-5">
+ <div class="py-5">
+ 	<h2 class="text-center">About us</h2>
+  </div>
+
+<div class="container-fluid">
+    <div class="row">
+    <div class="col-lg-6 col-md-6 col-12">
+     <img src="images/laptop.jpg" class="img-fluid aboutimg">
+    </div>
+    <div class="col-lg-6 col-md-6 col-12">
+     <h2 class="display-4"> Hello There All Of You!</h2>
+     <p class="py-3">In March this year, Molly-Mae explained during one of her vlogs that she'd "completely flipped [her] life upside down in terms of everything - exercise, food, water intake." She added, "I have been on a journey with my body, finding out what works for my body, what I need for my body to feel good." She also added</p>
+     <a href="about.php" class="btn btn-success">check more</a>
+    </div>
+    </div>
+</div>
+</section>	
+
+
+<section class="my-5">
+ <div class="py-5">
+ 	<h2 class="text-center">Our Services</h2>
+  </div>
+
+  <div class="container-fluid">
+        <div class="row">
+         <div class="col-lg-4 cold-md-4 cold-12">
+         <div class="card">
+         <img class="card-img-top" src="images/solo.jpg" alt="Card image">
+         <div class="card-body">
+         <h4 class="card-title">Beautiful Nature</h4>
+         <p class="card-text">Some example text.</p>
+         <a href="#" class="btn btn-primary">See Profile</a>
+
+              </div>
+            </div>
+        </div>
+        <div class="col-lg-4 cold-md-4 cold-12">
+         <div class="card">
+         <img class="card-img-top" src="images/solo.jpg" alt="Card image">
+         <div class="card-body">
+         <h4 class="card-title">Beautiful Nature</h4>
+         <p class="card-text">Some example text.</p>
+         <a href="#" class="btn btn-primary">See Profile</a>
+
+              </div>
+            </div>
+        </div> 	
+        <div class="col-lg-4 cold-md-4 cold-12">
+         <div class="card">
+         <img class="card-img-top" src="images/solo.jpg" alt="Card image">
+         <div class="card-body">
+         <h4 class="card-title">Beautiful Nature</h4>
+         <p class="card-text">Some example text.</p>
+         <a href="#" class="btn btn-primary">See Profile</a>
+
+              </div>
+            </div>
+        </div> 	 	
+      </div>	
+   </div>
+</section>
+
+
+
+<section class="my-5">
+ <div class="py-5">
+ 	<h2 class="text-center">Our Gallery</h2>
+  </div>
+
+  <div class="container-fluid">
+  <div class="row">
+       <div class="col-lg-4 col-md-4 col-12">
+       	  <img src="images/s.jpg" class="img-fluid pb-4"> 
+       </div>
+
+       <div class="col-lg-4 col-md-4 col-12">
+       	  <img src="images/s.jpg" class="img-fluid pb-4"> 
+       </div>
+
+       <div class="col-lg-4 col-md-4 col-12">
+       	  <img src="images/s.jpg" class="img-fluid pb-4"> 
+       </div>
+
+       <div class="col-lg-4 col-md-4 col-12">
+       	  <img src="images/s.jpg" class="img-fluid pb-4"> 
+       </div>
+
+       <div class="col-lg-4 col-md-4 col-12">
+       	  <img src="images/s.jpg" class="img-fluid pb-4"> 
+       </div>
+
+       <div class="col-lg-4 col-md-4 col-12">
+       	  <img src="images/s.jpg" class="img-fluid pb-4"> 
+       </div>
+
+       <div class="col-lg-4 col-md-4 col-12">
+       	  <img src="images/s.jpg" class="img-fluid pb-4"> 
+       </div>
+
+       <div class="col-lg-4 col-md-4 col-12">
+       	  <img src="images/s.jpg" class="img-fluid pb-4"> 
+       </div>
+
+       <div class="col-lg-4 col-md-4 col-12">
+       	  <img src="images/s.jpg" class="img-fluid pb-3"> 
+       </div>
+
+    </div>
+   </div>
+</section> 
+
+
+
+<section class="my-5">
+ <div class="py-5">
+ 	<h2 class="text-center">Our Gallery</h2>
+  </div>
+
+   <div class="w-50 m-auto">
+       <form action="userinfo.php" method="post">
+           <div class="form-group">
+             <label>Username</label>
+              <input type="text" name="user" autocomplete="off" class="form-control">
+           </div>
+
+           <div class="form-group">
+             <label>Email-id</label>
+              <input type="text" name="email" autocomplete="off" class="form-control">
+           </div>
+
+           <div class="form-group">
+             <label>Mobile</label>
+              <input type="text" name="mobile" autocomplete="off" class="form-control">
+           </div>
+
+           <div class="form-group">
+             <label>Comments</label>
+              <textarea class="form-control" name="comments">
+              </textarea> 
+           </div>
+           <button type="submit" class="btn btn-success">Submit</button>
+      </form>
+   </div>
+</section>
+
+
+
+<footer>
+  <p class="py-3 bg-dark text-white text-center">@gymfreakProduction</p>
+</footer>
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+
